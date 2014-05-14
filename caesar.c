3bd05205a8af990f99751e4de017d982b6a68968
Jordan Jambazov <jordan.jambazov@gmail.com>
2014-05-14 18:20:56 +0300

    Remove encode/decode option. Just use negative offset.

--- caesar.c
+++ caesar.c
@@ -1,18 +1,12 @@
 #include<stdio.h>
 
 int main(int argc, char **argv) {
-    if (argc != 3) {
-        printf("Usage: ./caesar <decode|encode> <offset>\n");
+    if (argc != 2) {
+        printf("Usage: ./caesar <offset>\n");
         return 1;
     }
 
-    char *codec = argv[1];
-    if (strcmp(codec, "encode") != 0 && strcmp(codec, "decode") != 0) {
-        printf("Wrong option %s. Use encode or decode.\n", codec);
-        return 1;
-    }
-
-    int offset = atoi(argv[2]);
+    int offset = atoi(argv[1]);
 
     char chr;
     char new_chr;
